d3f7aa052fb8ba28e12ab429eae1fa7d326325a1
Samuel Surgeon <ssurgeon00@icloud.com>
2020-08-15 12:17:01 +1000

    Add carousel JS

--- main.js
+++ main.js
@@ -1,6 +1,102 @@
+let itemClassName = 'carousel-article';
+let items = document.querySelectorAll(`.${itemClassName}`);
+let totalItems = items.length;
+let slide = 0;
+let moving = true;
+
 enableContactTicker();
 enableInformationSlideout();
-enableDraggableProjects(['#wv', '#hearth', '#sav']);
+initCarousel();
+//enableDraggableProjects(['#wv', '#hearth', '#sav']);
+
+function initCarousel() {
+  setInitialClasses();
+  setEventListeners();
+
+  moving = false;
+}
+
+function setInitialClasses() {
+  items[totalItems - 1].classList.add('prev');
+  items[0].classList.add('active');
+  items[1].classList.add('next');
+}
+
+function setEventListeners() {
+  let next = document.querySelectorAll('.carousel-button-next')[0];
+  let prev = document.querySelectorAll('.carousel-button-prev')[0];
+
+  next.addEventListener('click', moveNext);
+  next.addEventListener('click', movePrev);
+}
+
+function moveNext() {
+  if (!moving) {
+    if (slide === (totalItems - 1)) {
+      slide = 0;
+    } else {
+      slide++;
+    }
+
+    moveCarouselTo(slide);
+  }
+}
+
+function movePrev() {
+  if (!moving) {
+    if (slide === 0) {
+      slide = (totalItems - 1);
+    } else {
+      slide--;
+    }
+
+    moveCarouselTo(slide);
+  }
+}
+
+function disableInteraction() {
+  moving = true;
+
+  setTimeout(function() {
+    moving = false;
+  }, 500);
+}
+
+function moveCarouselTo(slide) {
+  if (!moving) {
+    disableInteraction();
+
+    let newPrevious = slide - 1,
+        newNext = slide + 1,
+        oldPrevious = slide - 2,
+        oldNext = slide + 2;
+
+    if ((totalItems - 1) > 3) {
+      if (newPrevious <= 0) {
+        oldPrevious = (totalItems - 1);
+      } else if (newNext >= (totalItems - 1)) {
+        oldNext = 0;
+      }
+
+      if (slide === 0) {
+        newPrevious = (totalItems - 1);
+        oldPrevious = (totalItems - 2);
+        oldNext = (slide + 1);
+      } else if (slide === (totalItems - 1)) {
+        newPrevious = (slide - 1);
+        newNext = 0;
+        oldNext = 1;
+      }
+
+      items[oldPrevious].className = itemClassName;
+      items[oldNext].className = itemClassName;
+
+      items[newPrevious].className = itemClassName + ' prev';
+      items[slide].className = itemClassName + ' active';
+      items[newNext].className = itemClassName + ' next';
+    }
+  }
+}
 
 function enableContactTicker() {
   window.addEventListener('load', () => {
@@ -38,6 +134,7 @@ function enableInformationSlideout() {
   });
 }
 
+/*
 function enableDraggableProjects(projectIDsArray) {
   projectIDsArray.forEach(ID => {
     let element = document.querySelector(ID);
@@ -80,3 +177,4 @@ function enableDraggableProjects(projectIDsArray) {
     }
   });
 }
+*/
